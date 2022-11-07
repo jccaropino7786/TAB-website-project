b27c157ac176a2853ee64543b476dbe6bc2b486a
john Caropino <jccaropino7786@live.com>
2022-11-06 17:31:33 -0800

    Updated Script Text, added some lists, fixed color

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const h1 = document.createElement("h1");
+h1.textContent = "John Curtis Caropino";
+
+document.querySelector("body").appendChild(h1);
+
 const h2 = document.createElement("h2");
-h2.textContent = "Your Bio goes here!";
+h2.textContent = `John Curtis aka "JC" Caropino has been working since the age of 15.  He has tackled challange after challange in his career and is looking forward to starting this new chapter in his life.  At 36 years old age comes expierience.  He has mastered time management skills with completing assigned tasks on time.  He has excelled in leadership roles, assigning tasks and scheculing deadlines to excede projected expectations.`;
 
 document.querySelector("body").appendChild(h2);
