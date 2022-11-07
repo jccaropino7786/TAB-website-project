4126475d4f460cde0f8e382446f4bacebb2b7ca6
john Caropino <jccaropino7786@live.com>
2022-11-07 08:04:05 -0800

    updated spelling errors

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ h1.textContent = "John Curtis Caropino";
 document.querySelector("body").appendChild(h1);
 
 const h2 = document.createElement("h2");
-h2.textContent = `John Curtis aka "JC" Caropino has been working since the age of 15.  He has tackled challange after challange in his career and is looking forward to starting this new chapter in his life.  At 36 years old age comes expierience.  He has mastered time management skills with completing assigned tasks on time.  He has excelled in leadership roles, assigning tasks and scheculing deadlines to excede projected expectations.`;
+h2.textContent = `John Curtis aka "JC" Caropino has been working since the age of 15, starting as a lifeguard, accumulating skills and added responsibility over the years.  He has tackled challanges and is looking forward to starting this new chapter in his life with software engineering.  With expeirience and critical thinking skills he is more than up for any challange ahead of him.  In Mastering time management skills it shows accountability and flexibility to handle all unseen problems that may arise.  He has excelled in leadership roles, assigning tasks and scheduling deadlines to excede projected expectations.`;
 
 document.querySelector("body").appendChild(h2);
